da2456daf7a391066bbaac188c789c6b9f046eba
Kevin <govkevin18@gmail.com>
2024-07-11 10:45:51 +0200

    Add black-scholes method

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,19 +1,22 @@
 import tkinter as tk
 
+from Models.black_scholes import BlackScholes
+
 
 class GUI:
 
     def __init__(self):
         None
 
-    def _select_model(self, model_name: str):
+    def _select_model(self, model_name: str, old_window: tk.Tk):
         """Select the model you want"""
+        old_window.destroy()
         model_window = tk.Tk()
         model_window.title(f"{model_name} Model")
         model_window.minsize(400, 300)
 
         if model_name == "Black-Scholes":
-            self._create_black_scholes_window(window=model_window)
+            self._create_black_scholes_window(model_window)
         else:
             label = tk.Label(model_window, text="NOT IMPLEMENTED !")
             label.grid(row=0, column=0, padx=10, pady=4)
@@ -22,9 +25,9 @@ class GUI:
         """Create the principal window"""
         principal_window = tk.Tk()
         principal_window.title("Select your model")
-        principal_window.minsize(300,200)
+        principal_window.minsize(300, 200)
 
-        button_bs = tk.Button(principal_window, text="Black Scholes Model", command=lambda: self._select_model("Black-Scholes"))
+        button_bs = tk.Button(principal_window, text="Black Scholes Model", command=lambda: self._select_model("Black-Scholes", old_window=principal_window))
         button_bs.pack(padx=20, pady=10)
 
         button_binomial = tk.Button(principal_window, text="Test Model", command=lambda: self._select_model("Binomial"))
@@ -38,17 +41,27 @@ class GUI:
         # Parameters to calculate the black_scholes model
         labels = ["Actual price of the underlying (S):", "Strike Price (K):", "Maturity (T):",
                   "Risk Free Rate (r):", "Volatility (sigma):"]
-        entries = [tk.Entry(window) for _ in labels]
+        defaults = [100, 100, 1, 0.05, 0.2]
+        entries = []
 
-        for i, (label_text, entry) in enumerate(zip(labels, entries)):
+        for i, (label_text, default_value) in enumerate(zip(labels, defaults)):
             label = tk.Label(window, text=label_text)
             label.grid(row=i, column=0, padx=10, pady=5, sticky="e")
+            entry = tk.Entry(window)
+            entry.insert(0, str(default_value))
             entry.grid(row=i, column=1, padx=10, pady=5, sticky="ew")
+            entries.append(float(entry.get()))
 
         entry_S, entry_K, entry_T, entry_r, entry_sigma = entries
 
+        black_scholes_obj = BlackScholes(strike=entry_K,
+                                         spot=entry_S,
+                                         maturity=entry_T,
+                                         risk_free=entry_r,
+                                         sigma=entry_sigma)
+
         # Button to calculate the price
-        button_calculate = tk.Button(window, text="Calculate price",)
+        button_calculate = tk.Button(window, text="Calculate price", command=black_scholes_obj.calculate_call_and_put())
         button_calculate.grid(row=5, columnspan=2, pady=10)
 
         # Display prices
@@ -58,7 +71,9 @@ class GUI:
         label_put_result = tk.Label(window, text="Price of put: ")
         label_put_result.grid(row=7, columnspan=2, pady=5)
 
+        label_call_result.config(text=f"Price of call: {black_scholes_obj.calculate_call():.2f}")
+        label_put_result.config(text=f"Price of put: {black_scholes_obj.calculate_put():.2f}")
+
         # Responsive behavior
         window.grid_columnconfigure(0, weight=1)
         window.grid_columnconfigure(1, weight=1)
-
--- Models/black_scholes.py
+++ Models/black_scholes.py
@@ -1,4 +1,34 @@
+import math
+
+from scipy.stats import norm
+
+
 class BlackScholes:
 
-    def __init__(self):
-        None
+    def __init__(self, *args, **kwargs):
+        self.strike = kwargs.get("strike")
+        self.spot = kwargs.get("spot")
+        self.maturity = kwargs.get("maturity")
+        self.risk_free = kwargs.get("risk_free")
+        self.sigma = kwargs.get("sigma")
+
+    def _black_scholes_elements(self):
+        """Function to calculate d1 and d2 for black scholes"""
+        d1 = (math.log(self.spot / self.strike) + (self.risk_free + (self.sigma**2)/2) * self.maturity) / (self.sigma * math.sqrt(self.maturity))
+        d2 = d1 - self.sigma * math.sqrt(self.maturity)
+        return d1, d2
+
+    def calculate_call(self):
+        """Function to calculate the call"""
+        d1, d2 = self._black_scholes_elements()
+        call_price = self.spot * norm.cdf(d1) - self.strike * math.exp(-self.risk_free * self.maturity) * norm.cdf(d2)
+        return call_price
+
+    def calculate_put(self):
+        """Function to calculate the put"""
+        d1, d2 = self._black_scholes_elements()
+        put_price = self.strike * math.exp(-self.risk_free * self.maturity) * norm.cdf(-d2) - self.spot * norm.cdf(-d1)
+        return put_price
+
+    def calculate_call_and_put(self):
+        return self.calculate_call(), self.calculate_put()
